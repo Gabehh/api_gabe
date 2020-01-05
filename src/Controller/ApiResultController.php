9922e9b1a366b31c7350b0f95da5e497ee4e2417
Gabriel Enrique Macho <gabe3195@gmail.com>
2020-01-05 13:16:58 +0100

    ADD ENDPOINT POST RESULT

--- src/Controller/ApiResultController.php
+++ src/Controller/ApiResultController.php
@@ -11,6 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -79,7 +80,82 @@ class ApiResultController extends AbstractController
 
         return Utils::apiResponse(
             Response::HTTP_OK,
-            [ 'user' => $result ],
+            [ 'results' => $result ],
+            $format
+        );
+    }
+
+
+    /**
+     * POST Result
+     *
+     * @param Request $request request
+     * @return Response
+     * @Route(
+     *     ".{_format}",
+     *     defaults={"_format": null},
+     *     requirements={
+     *         "_format": "json|xml"
+     *     },
+     *     methods={ Request::METHOD_POST },
+     *     name="post"
+     * )
+     *
+     * @Security(
+     *     expression="is_granted('IS_AUTHENTICATED_FULLY')",
+     *     statusCode=401,
+     *     message="Invalid credentials."
+     * )
+     */
+    public function createResult(Request $request): Response
+    {
+        // Puede crear un result sólo si tiene ROLE_ADMIN
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            throw new HttpException(   // 403
+                Response::HTTP_FORBIDDEN,
+                "`Forbidden`: you don't have permission to access"
+            );
+        }
+
+
+        $data = json_decode(
+            $request->getContent(),
+            true
+        );
+
+        //$body = $request->getContent();
+        //$postData = json_decode($body, true);
+        $format = Utils::getFormat($request);
+
+
+        if (!isset($data['result'],$data['userId'],$data['time'])) {
+            // 422 - Unprocessable Entity Faltan datos
+            $message = new Message(Response::HTTP_UNPROCESSABLE_ENTITY, Response::$statusTexts[422]);
+            return Utils::apiResponse(
+                $message->getCode(),
+                [ 'message' => $message ],
+                $format
+            );
+        }
+
+        // hay datos -> procesarlos
+
+        $result = new Result(
+            $data['result'],
+            $data['userId'],
+            $data['time']
+        );
+        // roles
+        if (isset($data['comment'])) {
+            $result->setComment($data['comment']);
+        }
+
+        $this->entityManager->persist($result);
+        $this->entityManager->flush();
+
+        return Utils::apiResponse(
+            Response::HTTP_CREATED,
+            [ 'result' => $result ],
             $format
         );
     }
